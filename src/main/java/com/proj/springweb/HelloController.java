57507b469970c3f830fa0c5e42b72733fa586a8a
mirian228 <pariksini@mail.ru>
2023-04-09 22:11:41 +0400

    Basic Spring MVC hello world app

---
+++ src/main/java/com/proj/springweb/HelloController.java
@@ -0,0 +1,16 @@
+package com.proj.springweb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+	
+	@GetMapping("/hello-world")
+	public String sayHello() {
+		return "hello_world";
+	}
+	
+	
+	
+}
