3d5fdaf86b20346c42d1aa0095b7b8f808b9c748
mirian228 <pariksini@mail.ru>
2023-04-09 22:41:25 +0400

    Configuration using java classes instead of xml files

---
+++ src/main/java/com/proj/springweb/config/MySpringMvcDispatcherServletInitializer.java
@@ -0,0 +1,23 @@
+package com.proj.springweb.config;
+
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+//Same as web.xml
+public class MySpringMvcDispatcherServletInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+
+	@Override
+	protected Class<?>[] getRootConfigClasses() {
+		return null;
+	}
+
+	@Override
+	protected Class<?>[] getServletConfigClasses() {
+		return new Class[] {SpringConfig.class};
+	}
+
+	@Override
+	protected String[] getServletMappings() {
+		return new String[] {"/"};
+	}
+
+}
