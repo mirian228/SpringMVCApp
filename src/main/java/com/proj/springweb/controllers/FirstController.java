0974e7391ba2986913d5ce57ee3d9c44c18fb7bd
mirian228 <pariksini@mail.ru>
2023-04-11 14:04:53 +0400

    Implemented calculator page with dynamic data using thymeleaf, GET method using query params now can do addition, substraction, multiplication,division and return value dynamically on web page

--- src/main/java/com/proj/springweb/controllers/FirstController.java
+++ src/main/java/com/proj/springweb/controllers/FirstController.java
@@ -1,6 +1,7 @@
 package com.proj.springweb.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -8,20 +9,22 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 @RequestMapping("/first")
 public class FirstController {
-	
-	
-	//@RequestParam() annotation is better approach than creating HttpServletRequest object --> 
+
+	// @RequestParam() annotation is better approach than creating
+	// HttpServletRequest object -->
 	// --> To get Request Body from get Method
 	// Now the url will be /first/hello?name
 	@GetMapping("/hello")
-	public String helloPage(@RequestParam("name") String name, @RequestParam("surname") String surname) {
-		
-		System.out.println("Hello, " + name + " " + surname );
-		
+	public String helloPage(@RequestParam(value = "name", required = false) String name,
+			@RequestParam(value = "surname", required = false) String surname, Model model) {
+		model.addAttribute("message", "Hello, " + name + " " + surname);
 		return "first/hello";
 	}
-	
-	/*// This approach requires more core and we create object which stores all information which we may not need
+
+	/*
+	 * // This approach requires more core and we create object which stores all
+	 * information which we may not need
+	 * 
 	 * @GetMapping("/hello") public String helloPage(HttpServletRequest request) {
 	 * String name = request.getParameter("name"); return "first/hello"; }
 	 */
@@ -29,6 +32,32 @@ public class FirstController {
 	@GetMapping("/goodbye")
 	public String goodByePage() {
 		return "first/goodbye";
+	}
+
+	@GetMapping("/calculator")
+	public String calculatorPage(@RequestParam(value = "a", required = false) Integer a,
+			@RequestParam(value = "b", required = false) Integer b,
+			@RequestParam(value = "action", required = false) String action, Model model) {
+
+		if ((action == null) == false) {
+
+			if (action.equalsIgnoreCase("multiplication")) {
+				model.addAttribute("message", "Result of multiplication = " + (a * b));
+			}
+			if (action.equalsIgnoreCase("addition")) {
+				model.addAttribute("message", "Result of addition = " + (a + b));
+			}
+			if (action.equalsIgnoreCase("substraction")) {
+				model.addAttribute("message", "Result of substraction = " + (a - b));
+			}
+			if (action.equalsIgnoreCase("division")) {
+				model.addAttribute("message", "Result of division = " + (a / b));
+			} else {
+				model.addAttribute("message", "No query parameters have been used");
+			}
+		}
+
+		return "first/calculator";
 	}
 
 }
